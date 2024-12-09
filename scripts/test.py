45608048ce5380bcef2688d23e6cdc6ccee35c73
Kahl-d <email.khalidmkhan@gmail.com>
2024-12-09 07:08:30 -0800

    data push

---
+++ scripts/test.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+data = pd.read_csv('../data/data_class.csv')
+print(data.head(10))
---
+++ scripts/test_env.py
@@ -0,0 +1,10 @@
+import lime
+import torch
+import numpy as np
+import pandas as pd
+import sklearn
+import transformers
+
+print("Environment setup is successful!")
+print("Torch version:", torch.__version__)
+print("Transformers version:", transformers.__version__)
